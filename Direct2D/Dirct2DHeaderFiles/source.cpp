20e0397b8340a15455a21d3be5820be450b0338f
ZTI <zhaotianff@163.com>
2019-08-20 09:49:22 +0800

    First commit

---
+++ Direct2D/Dirct2DHeaderFiles/source.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+/*
+d2d1.h	
+Defines C and C++ versions of the primary Direct2D API.
+
+d2d1helper.h	D
+efines C++ helper functions, classes, and structures.
+
+d2dbasetypes.h	
+Defines drawing primitives for Direct2D, such as points and rectangles. This header is included with d2d1.h.
+
+d2derr.h	
+Defines the error codes for Direct2D. This header is included with d2d1.h.
+
+d2d1_1.h	
+Defines C and C++ versions of the primary Direct2D API for Windows 8 and later.
+
+d2d1_1helper.h	
+Defines C++ helper functions, classes, and structures for Windows 8 and later.
+
+d2d1effects.h	
+Defines C and C++ versions of the image effects part of the Direct2D API for Windows 8 and later.
+
+d2d1effecthelpers.h	
+Defines C++ helper functions, classes, and structures of the image effects part of the Direct2D API for Windows 8 and later.
+*/
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Direct2D
 Direct2D demo
+
+## Development Environment
+Visual Studio 2019 
+>To build this project,Windows SDK and Visual C++ 142 toolset is required
+>Starting with Windows 8, the DirectX SDK is included as part of the Windows SDK.
+<p align="center">
+ <img align="center" alt="Windows SDK" src="https://github.com/zhaotianff/CSharpCrawler/blob/master/CSharpCrawler/ScreenShots/2.png" />
+</p>
+<p align="center">
+ <img align="center" alt="VC142" src="https://github.com/zhaotianff/CSharpCrawler/blob/master/CSharpCrawler/ScreenShots/2.png" />
+</p>
+
+## Demo List
+* Direct2DQuickStart
+
+## License
+[MIT License](LICENSE).
+
+
+
